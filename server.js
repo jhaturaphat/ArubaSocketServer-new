ff8fb2be18c07b926772e5f8d744f5c5582239cc
oLuciaZo <charuraks@gmail.com>
2021-12-10 17:57:41 +0700

    Update to insert MySQL
    Shaw

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var mysql = require('mysql');
 const WebSocket = require('ws');
 const protobuf = require('protobufjs');
 const aruba_telemetry_proto = require('./aruba_iot_proto.js').aruba_telemetry;
@@ -19,14 +20,26 @@ wss.on('connection', function connection(ws) { // สร้าง connection
     if(obj["reported"]==null){
       console.log("Aruba Websocket Established");
     }else{
+      console.log(myobj);
       console.log(obj["reporter"]["name"]);
-      console.log(obj["reporter"]["ipv4"]);
+      //console.log(obj["reporter"]["ipv4"]);
       console.log(obj["reported"]);
-      beacon(obj["reported"]);
-    if(obj["reported"]["deviceClass"]=='arubaTag'){
-      console.log(obj["reported"]["rssi"]);
-      console.log(obj["reported"]["sensors"]);
-    }
+      //console.log(obj.reported);
+      add_sensors(obj["reporter"]["name"], obj.reported);
+      //console.log(obj.reported[0]["deviceClass"]);
+      /*let count=0;
+      for (k of obj.reported) {
+        console.log(obj.reported[count]["deviceClass"]);
+        console.log("===================================");
+        if(obj.reported[count]["deviceClass"].includes('iBeacon')==true){
+          console.log(obj.reported[count]["beacons"][0]['ibeacon']['uuid']);
+          console.log('iBaecon');
+        }
+        if(obj.reported[count]["deviceClass"]=='arubaTag'){
+          console.log('ArubaTag');
+        }
+        count +=1;
+      }*/
     }
     //console.log(telemetryReport.body);
   });
@@ -45,3 +58,81 @@ async function beacon(report){
     console.log(k.beacons);
 }
 }
+
+function add_sensors(location, sensor){
+  let count=0;
+  for (k of sensor) {
+    console.log("===================================");
+    if(sensor[count]["deviceClass"].includes('iBeacon')==true&&sensor[count]["deviceClass"].includes('arubaBeacon')==false&&sensor[count]['rssi']!=null){
+      let sql = "";
+      let param = {};
+      param[0] = sensor[count]['mac'];
+      param[1] = 'iBeacon';
+      console.log(sensor[count]['rssi']['last']);
+      param[2] = sensor[count]['rssi']['last'];
+      param[3] = new Date().toISOString();
+      param[4] = sensor[count]["beacons"][0]['ibeacon']['major'];
+      param[5] = sensor[count]["beacons"][0]['ibeacon']['minor'];
+      sql = "INSERT INTO sensors (s_mac_address, s_device_type, s_minor, s_major, s_rssi, s_timestamp, s_location) VALUES ('"+param[0]+"', '"+param[1]+"', "+param[5]+", "+param[4]+", "+param[2]+", "+'NOW()'+", '"+location+"')";
+      add_db(sql);
+    }
+    else if(sensor[count]["deviceClass"]=='arubaTag'&&sensor[count]['rssi']!=null){
+      let sql = "";
+      let param = {};
+      param[0] = sensor[count]['mac'];
+      param[1] = 'arubaTag';
+      console.log(sensor[count]['rssi']['last']);
+      param[2] = sensor[count]['rssi']['last'];
+      param[3] = new Date().toISOString();
+      param[4] = sensor[count]['sensors']['battery'];
+      sql = "INSERT INTO sensors (s_mac_address, s_device_type, s_battery, s_rssi, s_timestamp, s_location) VALUES ('"+param[0]+"', '"+param[1]+"', "+param[4]+", "+param[2]+", "+'NOW()'+", '"+location+"')";
+      add_db(sql);
+
+    }else if(sensor[count]["deviceClass"]=='eddystone'&&sensor[count]['rssi']!=null){
+      let sql = "";
+      let param = {};
+      param[0] = sensor[count]['mac'];
+      param[1] = 'eddystone';
+      param[2] = sensor[count]['rssi']['last'];
+      param[3] = new Date().toISOString();
+      param[4] = sensor[count]['sensors']['temperatureC'];
+      sql = "INSERT INTO sensors (s_mac_address, s_device_type, s_dynamic_value, s_rssi, s_timestamp, s_location) VALUES ('"+param[0]+"', '"+param[1]+"', "+param[4]+", "+param[2]+", "+'NOW()'+", '"+location+"')";
+      add_db(sql);
+    }
+    count +=1;
+  }
+}
+
+function add_db(sql){
+  var con = mysql.createConnection({
+    host: "10.5.255.31",
+    user: "root",
+    password: "password",
+    database: "iot"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("DB Connected!");
+    //var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Insert Data >> "+sql);
+      con.end();
+      console.log("Closed Connections");
+    });
+  });
+
+}
+
+/*
+console.log(obj.reported[count]["deviceClass"]);
+    console.log("===================================");
+    if(obj.reported[count]["deviceClass"].includes('iBeacon')==true){
+      console.log(obj.reported[count]["beacons"][0]['ibeacon']['uuid']);
+      console.log('iBaecon');
+    }
+    if(obj.reported[count]["deviceClass"]=='arubaTag'){
+      console.log('ArubaTag');
+    }
+*/
--- test.js
+++ test.js
@@ -1,16 +1,82 @@
-const data1 = [{
-    "meta":{
-    "version":"1","access_token":"1234","nbTopic":"telemetry"
-    },"reporter":{
-    "name":"20:4c:03:8b:86:fe","mac":"204c038b86fe","ipv4":"10.10.99.3","hwType":"AP-303H","swVersion":"8.7.1.6-8.7.1.6","swBuild":"81786","time":"1637655823"
-    },"reported":[{
-    "mac":"f008d155b4fe","deviceClass":["iBeacon","eddystone"],"model":"iBeacon","lastSeen":"1637655820","bevent":{"event":"update"},"rssi":{"last":-84},"beacons":[{"ibeacon":{"uuid":"783a69707094d0863b4a9bf94e555241","major":816,"minor":6,"power":0},"eddystone":{"power":0}}],"sensors":{"temperatureC":18,"voltage":3.299999952316284},"stats":{"uptime":"710","adv_cnt":"24","frame_cnt":7}},
-    {"mac":"a0e6f8515eff","deviceClass":["arubaTag"],"firmware":{"bankA":"1.2-15"},"assetId":"0000-0000-0000","lastSeen":"1637655821","bevent":{"event":"update"},"rssi":{"last":-69},"sensors":{"battery":83},"stats":{"uptime":"82350","frame_cnt":3},"vendorName":"Aruba"}]}];
+var mysql = require('mysql');
+const data1 = {"meta":{"version":"1","access_token":"1234","nbTopic":"telemetry"},"reporter":{"name":"Gaming-Room","mac":"204c038b86fe","ipv4":"192.168.1.155","ipv6":"fe80::224c:3ff:fe8b:86fe","hwType":"AP-303H","swVersion":"8.7.1.6","swBuild":"81786","time":"1639126691"},
+"reported":[
+  {"mac":"f008d155b65e","deviceClass":["iBeacon"],"model":"iBeacon","lastSeen":"1639126660","bevent":{"event":"update"},"beacons":[{"ibeacon":{"uuid":"783a69707094d0863b4a9bf94e555241","major":214,"minor":87,"power":0}}],"stats":{"frame_cnt":0}},
+  {"mac":"f008d155b4fe","deviceClass":["eddystone"],"lastSeen":"1639126689","bevent":{"event":"update"},"rssi":{"last":-77},"beacons":[{"eddystone":{"power":0}}],"sensors":{"temperatureC":71,"voltage":3.299999952316284},"stats":{"uptime":"67420","adv_cnt":"75","frame_cnt":7}},
+  {"mac":"a0e6f8515eff","deviceClass":["arubaTag"],"firmware":{"bankA":"1.2-15"},"assetId":"0000-0000-0000","lastSeen":"1639126678","bevent":{"event":"update"},"rssi":{"last":-69},"sensors":{"battery":92},"stats":{"uptime":"84450","frame_cnt":3},"vendorName":"Aruba"},
+  {"mac":"0cae7dbffd5f","deviceClass":["iBeacon","arubaBeacon"],"model":"BT-AP300H","firmware":{"bankA":"0.0-0","bankB":"1.2-42"},"lastSeen":"1639125487","bevent":{"event":"update"},"beacons":[{"ibeacon":{"uuid":"4152554ef99b4a3b86d0947070693a78","major":0,"minor":0,"power":-56}}],"txpower":14,"sensors":{"battery":-101},"stats":{"uptime":"10740","frame_cnt":0},"vendorName":"Aruba"}]}
   
-/*
+
 
     let myobj = JSON.stringify(data1);
     let obj = JSON.parse(myobj);
+    //console.log(obj);
+    
+    if(obj["reported"]==null){
+      console.log("Aruba Websocket Established");
+    }else{
+      console.log(obj["reporter"]["name"]);
+      console.log(obj["reporter"]["ipv4"]);
+      sensor = obj.reported;
+      //console.log(obj["reported"]);
+      //console.log(obj.reported[0]["deviceClass"]);
+      let count=0;
+      for (k of sensor) {
+        console.log(sensor[count]["deviceClass"]);
+        console.log("===================================");
+        if(sensor[count]["deviceClass"].includes('iBeacon')==true&&sensor[count]["deviceClass"].includes('arubaBeacon')==false){
+          console.log(sensor[count]['mac']);
+          if(sensor[count]['rssi']==null){
+            console.log("Skipped");
+          }else{
+            console.log(sensor[count]['rssi']['last']);
+          }
+          
+          
+        }
+        else if(sensor[count]["deviceClass"]=='arubaTag'){
+          let mac = sensor[count]['mac'];
+          let devicetype = 'arubaTag';
+          let rssi = mac = sensor[count]['rssi']['last'];
+          console.log(rssi);
+          let date = new Date().toISOString();
+          let battery = sensor[count]['sensors']['battery'];
+          console.log(battery);
+        }
+        else if(sensor[count]["deviceClass"]=='eddystone'){
+          let mac = sensor[count]['mac'];
+          let devicetype = 'eddystone';
+          let rssi = mac = sensor[count]['rssi'];
+          let date = new Date().toISOString();
+          let dyn_value = sensor[count]['sensors']['temperatureC'];
+          console.log(dyn_value);
+          let v = {};
+          v[0] = "a";
+          v[1] = "b";
+          console.log(v[1],v[0]);
+        }
+        count +=1;
+      }
+    }
+
+    
+/*
+var con = mysql.createConnection({
+  host: "10.5.255.31",
+  user: "root",
+  password: "password"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.end();
+});*/
+
+
+
+
+/*
     let abc = "";
     for (k of obj) {
        console.log(k.reported);
@@ -25,12 +91,8 @@ const data1 = [{
               console.log(properties[keys[i]]);
                   this[keys[i]] = properties[keys[i]];
   }
+
 */
-var a = 0;
-for(var i = 0; i < 10;i++){
-  a +=1;
-    console.log(a);
-}
   //================output================//
   /*
   const data = data1.filter(d => d.mac == "f008d155b4fe");
